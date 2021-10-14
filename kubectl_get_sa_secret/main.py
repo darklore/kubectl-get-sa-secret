98f5d432c4ab6a8e04832b97ac3db9cae3b3778c
darklore <zodiac.brave.story@gmail.com>
2021-10-14 14:48:42 +0900

    find from serviceaccount

--- kubectl_get_sa_secret/main.py
+++ kubectl_get_sa_secret/main.py
@@ -17,18 +17,35 @@ import click
     required=True,
     help="Service Account name of secret you want to get",
 )
-def main(namespace: str, service_account: str):
-    cmd = "kubectl get secrets "
-
+def main(namespace: str, service_account: str, index=0):
+    cmd = f"kubectl get serviceaccount {service_account}"
     if namespace is not None and len(namespace) > 0:
-        cmd += f'-n "{namespace}" '
+        cmd += f' -n "{namespace}"'
+    cmd += f" -o jsonpath='{{.secrets[{index}].name}}'"
 
-    cmd += f"-o jsonpath=\"{{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='{service_account}')].data.token}}\""
+    ret = subprocess.run(cmd, capture_output=True, shell=True, check=True)
+    secret = ret.stdout.decode("utf-8")
 
-    # print(cmd)
+    cmd = f"kubectl get secret {secret}"
+    if namespace is not None and len(namespace) > 0:
+        cmd += f' -n "{namespace}"'
+    cmd += " -o jsonpath='{.data.token}'"
+
     ret = subprocess.run(cmd, capture_output=True, shell=True, check=True)
     token = ret.stdout.decode("utf-8")
     print(token, end="")
+
+    # cmd = "kubectl get secrets "
+
+    # if namespace is not None and len(namespace) > 0:
+    #     cmd += f'-n "{namespace}" '
+
+    # cmd += f"-o jsonpath=\"{{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='{service_account}')].data.token}}\""
+
+    # # print(cmd)
+    # ret = subprocess.run(cmd, capture_output=True, shell=True, check=True)
+    # token = ret.stdout.decode("utf-8")
+    # print(token, end="")
 
 
 if __name__ == "__main__":
